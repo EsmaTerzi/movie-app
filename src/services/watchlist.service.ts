abf1b290c3f97f61b735fbb93aaecb03750a5f98
EsmaTerzi <n.esmaterzi@gmail.com>
2025-12-12 14:20:38 +0300

    feat

--- src/services/watchlist.service.ts
+++ src/services/watchlist.service.ts
@@ -7,7 +7,6 @@ export const watchlistService = {
         const response = await axiosInstance.post('/watchlists', {name });
         return response.data;
     },
-
     getUserWatchlists: async (userId: string): Promise<any[]> => {
         const response = await axiosInstance.get<any[]>(`/users/${userId}/watchlists`);
         return response.data;
