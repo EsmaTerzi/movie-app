88646253df3107639b6ac5477f1892096b25435d
EsmaTerzi <n.esmaterzi@gmail.com>
2025-12-12 14:10:47 +0300

    chore: feature

---
+++ src/services/watchlist.service.ts
@@ -0,0 +1,17 @@
+import axiosInstance from '@/utils/axios';
+
+export const watchlistService = {
+
+    // Kullanıcı izleme listesi oluştur
+    createWatchlist: async (name: string): Promise<any> => {
+        const response = await axiosInstance.post('/watchlists', {name });
+        return response.data;
+    },
+
+    getUserWatchlists: async (userId: string): Promise<any[]> => {
+        const response = await axiosInstance.get<any[]>(`/users/${userId}/watchlists`);
+        return response.data;
+    }
+ 
+
+};
